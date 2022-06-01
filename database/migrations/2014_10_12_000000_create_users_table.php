151297f61cc3c425be11617142e63ef695347341
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2022-06-01 22:49:11 +0700

    finis

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
- 
+            $table->string('email')->unique()->nullable();
+
             $table->integer('role')->nullable();
 
             $table->timestamp('email_verified_at')->nullable();
